3c0f4f8c0f6e9a2f688ea53e2602d86d05df1b26
Tom <tom@andersonweb.co.uk>
2017-03-14 00:26:41 +0000

    Added json into default route

--- LRNAPI/routes/index.js
+++ LRNAPI/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+
+    res.json([{
+        "id": "16296355",
+        "url": "r16296355_manchester",
+        "text": "Manchester"
+    },
+        {
+            "id": "16844357",
+            "url": "k16844357_manchester-city-centre",
+            "text": "Manchester City Centre"
+        }]);   
 });
 
 module.exports = router;
